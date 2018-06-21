95e0a9bab3d4303d5c30cc9d4272581d09074181
rnjstjdqhd3939 <rnjstjdqhd3939@gmail.com>
2018-06-21 11:54:55 +0900

    test

---
+++ admin-server/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
